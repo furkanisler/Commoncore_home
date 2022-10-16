37645ecbce387fbea4f2a938735834485931f996
Muhammed Furkan Isler <misler@k2m09s02.42kocaeli.com.tr>
2022-10-16 18:09:01 +0300

    all ok but itoa

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -12,26 +12,14 @@
 
 #include"libft.h"
 
-t_list	*ft_listnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
-	size_t	len;
-
-	new = NULL;
+	
 	new = (t_list *)malloc(sizeof(t_list));
-	if (new)
-	{
-		if (content == NULL)
-			new->content = NULL;
-		else
-		{
-			len = ft_strlen(content);
-			new->content = (void *)malloc(len + 1);
-			if (new->content == NULL)
-				return (0);
-			ft_memcpy(new->content, content, len);
-		}
-		new->next = NULL;
-	}
+	if (!new)
+		return (NULL);
+	new -> content = content;
+	new -> next = NULL;
 	return (new);
 }
